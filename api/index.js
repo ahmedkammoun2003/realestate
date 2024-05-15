7d7c8458fb402ad533b1020f4af5c6b30d7745cd
ahmedkamoun2003 <ahmed.kamoun@ensi-uma.tn>
2024-05-15 13:24:33 +0100

    database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log(`Database connected...`);
+}).catch(
+    err => console.log(err));
 app.listen(3000, () => {
-    console.log(`Server started on port 3000...`);
+    console.log(`Server started on port 3000`);
 });
