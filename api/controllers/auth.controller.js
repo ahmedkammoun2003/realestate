45f4485ea649fea38b99106426c04735732c3bc2
ahmedkamoun2003 <ahmed.kamoun@ensi-uma.tn>
2024-05-21 20:17:53 +0100

    signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 import { ErrorHandler } from "../utils/error.js";
+import  Jwt  from "jsonwebtoken";
 export const signup = async (req,res,next)=>{
     const {username,email,password}=req.body;
     const hasherPassword = bcryptjs.hashSync(password,10);
@@ -8,6 +9,25 @@ export const signup = async (req,res,next)=>{
     try {
         await newUser.save();
         res.status(201).json({message:"User created successfully"});
+    } catch (error) {
+        next(error);
+    }
+};
+export const signin = async (req,res,next)=>{
+    const {email,password}=req.body;
+    try {
+        const user = await User.findOne({email});
+        if(!user){
+            return next(ErrorHandler(404,"User not found"));
+        }
+        if(!bcryptjs.compareSync(password,user.password)){
+            return next(ErrorHandler(401,"Invalid credentials"));
+        }
+        const {password:pass,...rest}=user._doc;
+        const token = Jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1d"});
+        res.cookie("access_token",token,{httpOnly:true})
+        .status(200)
+        .json(rest);
     } catch (error) {
         next(error);
     }
