76d68554fa3303857504fd79d435682153344ba0
ahmedkamoun2003 <ahmed.kamoun@ensi-uma.tn>
2024-06-02 22:23:59 +0100

    end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter,Route,Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
+import  Home  from "./pages/Home";
 import { SignIn } from "./pages/SignIn";
 import {  SignUp } from "./pages/SignUp";
-import { About } from "./pages/About";
+import  About  from "./pages/About";
 import { Profile } from "./pages/Profile";
 import  CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
