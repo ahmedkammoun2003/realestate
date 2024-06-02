8831ab7ca71d7cf51d623c565dedaabf442bd39d
ahmedkamoun2003 <ahmed.kamoun@ensi-uma.tn>
2024-06-02 22:36:29 +0100

    deploy

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import  express  from "express";
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
@@ -6,28 +6,36 @@ import authRoute from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log(`Database connected...`);
-}).catch(
-    err => console.log(err));
+  })
+  .catch((err) => console.log(err));
 app.listen(3000, () => {
-    console.log(`Server started on port 3000`);
+  console.log(`Server started on port 3000`);
 });
+const __dirname = path.resolve();
 app.use(cookieParser());
-app.use('/api',userRoute);
-app.use('/api/auth', authRoute);
-app.use('/api/listings', listingRouter);
-app.use((err,req,res,next)=>{
-    const statuscode = err.statuscode || 500;
-    const message = err.message || "Something went wrong";
-    return res.status(statuscode).json({
-        success: false,
-        statuscode,
-        message
-    });
+app.use("/api", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/listings", listingRouter);
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'/client/dist/index.html'));
+});
+app.use((err, req, res, next) => {
+  const statuscode = err.statuscode || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(statuscode).json({
+    success: false,
+    statuscode,
+    message,
+  });
 });
